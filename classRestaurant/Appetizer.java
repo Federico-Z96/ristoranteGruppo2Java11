9373f896553aa3cb766811224b9b150b76b502bd
MichaelB94 <124793101+MichaelB94@users.noreply.github.com>
2023-02-28 18:51:32 +0100

    Appetizer mod

--- classRestaurant/Appetizer.java
+++ classRestaurant/Appetizer.java
@@ -1,2 +1,39 @@
-package classRestaurant;public class Appetizer {
+package classRestaurant;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Appetizer {
+    private String name;
+    private double price;
+
+
+
+
+    public Appetizer(String name, double price) {
+        this.name = name;
+        this.price = price;
+        System.out.println(name+price);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
 }
+
--- classRestaurant/main.java
+++ classRestaurant/main.java
@@ -6,14 +6,24 @@ import java.util.List;
 
 public  class main {
     public static void main(String[] args) {
-      List<Beverage> beverages = Arrays.asList(
-              new Beverage("Vino ","Rosso della casa ",10),
-              new Beverage("Vino ","Bianco della casa ", 11),
-              new Beverage("Birra ","Bionda ", 5),
-              new Beverage("Birra ","Rossa ", 7),
-              new Beverage("Birra ","Ipa ", 7.5)
-      );
-        }
+        List<Beverage> beverages = Arrays.asList(
+                new Beverage("Vino ", "Rosso della casa ", 10),
+                new Beverage("Vino ", "Bianco della casa ", 11),
+                new Beverage("Birra ", "Bionda ", 5),
+                new Beverage("Birra ", "Rossa ", 7),
+                new Beverage("Birra ", "Ipa ", 7.5)
+        );
+
+        List<Appetizer> menuAppetizer = Arrays.asList(
+                new Appetizer("Sformatino di verdure ", 4.99),
+                new Appetizer("Pinzimonio ", 4.50),
+                new Appetizer("Verdure pastellate ", 2.50),
+                new Appetizer("Ratatouille ", 3.50),
+                new Appetizer("Cavolo Gratinato ", 3.99)
+        );
+
     }
+
+}
 
 
