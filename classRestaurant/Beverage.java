caa8e9bc3270896b89982aedf0fa179a21d8ea47
federico <zac.fede96@gmail.com>
2023-02-28 18:27:30 +0100

    addbeverage

--- classRestaurant/Beverage.java
+++ classRestaurant/Beverage.java
@@ -1,4 +1,47 @@
 package classRestaurant;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Beverage {
+    private String name;
+    private double price;
+    private String type;
+
+
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    public Beverage(String type,String name,double price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+        System.out.println(type + name + price);
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- classRestaurant/main.java
+++ classRestaurant/main.java
@@ -1,4 +1,19 @@
 package classRestaurant;
 
-public class main {
-}
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public  class main {
+    public static void main(String[] args) {
+      List<Beverage> beverages = Arrays.asList(
+              new Beverage("Vino ","Rosso della casa ",10),
+              new Beverage("Vino ","Bianco della casa ", 11),
+              new Beverage("Birra ","Bionda ", 5),
+              new Beverage("Birra ","Rossa ", 7),
+              new Beverage("Birra ","Ipa ", 7.5)
+      );
+        }
+    }
+
+
