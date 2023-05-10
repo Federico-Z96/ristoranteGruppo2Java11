1317fc6bed8c5c86dd981b7166d2ceaece831f3b
federico <zac.fede96@gmail.com>
2023-05-10 11:20:20 +0200

    aggiunta tabella beverage

--- database.sql
+++ database.sql
@@ -12,6 +12,12 @@ PRICE DOUBLE not null,
 ALLERGENS VARCHAR(255),
 ITS_FROZEN BOOLEAN not NULL
 );
+CREATE TABLE Beverage(
+NAME VARCHAR(255)not null,
+PRICE DOUBLE not null,
+ALLERGENS VARCHAR(255),
+TYPE VARCHAR(255);
+);
 
 insert into FIRST_DISHES (NAME, PRICE,ALLERGENS,ITS_FROZEN,ITS_PRECOOCKED)
 values
@@ -20,7 +26,7 @@ values
 ('Passata di Verdure',3,NULL,false,TRUE),
 ('Gnocchi alla romana',6.5,'Lactos',false,TRUE);
 
-insert into FIRST_DISHES (NAME, PRICE,ALLERGENS,ITS_FROZEN)
+insert into Appetizer (NAME, PRICE,ALLERGENS,ITS_FROZEN)
 values
 ('Sformatino di verdure',4.99,'Eggs and Derivates',false),
 ('Pinzimonio',4.50,null,false),
@@ -28,6 +34,13 @@ values
 ('Ratatouille',7.50,null,false),
 ('Cavolo Gratinato',3.99,'Lactos',false);
 
+insert into Beverage (NAME, PRICE, ALLERGENS, TYPE)
+values
+('Chianti',10,false,'RED_WINE'),
+('Prosecco',1,false,'WHITE_WINE'),
+('Heinekken',5,true,'BLONDE'),
+('Red Erik',7,true,'RED'),
+('Indian Pale Ale',7.5,true,'IPA');
 -- aggiuntive
 -- Obbiettivo: con questo file io ho la parte database che ci servirà per iniziare a programmare
 select * FROM INGREDIENT;
